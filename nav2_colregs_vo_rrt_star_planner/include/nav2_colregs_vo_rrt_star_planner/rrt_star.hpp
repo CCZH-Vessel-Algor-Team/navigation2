87b1499e274cca18e76d58606ce04eafdd855b99
vectorwang <vectorwang@hotmail.com>
2026-07-06 14:06:22 +0800

    feat: add VO-RRT* planner plugin with /get_avoidance_point service integration

---
+++ nav2_colregs_vo_rrt_star_planner/include/nav2_colregs_vo_rrt_star_planner/rrt_star.hpp
@@ -0,0 +1,88 @@
+#ifndef NAV2_COLREGS_VO_RRT_STAR_PLANNER__RRT_STAR_HPP_
+#define NAV2_COLREGS_VO_RRT_STAR_PLANNER__RRT_STAR_HPP_
+
+#include <functional>
+#include <random>
+#include <vector>
+
+namespace nav2_costmap_2d { class Costmap2D; }
+
+namespace nav2_colregs_vo_rrt_star_planner
+{
+
+struct RRTStarNode
+{
+  double x, y;
+  int parent_idx;           // -1 for root
+  double cost_from_root;
+
+  RRTStarNode() : x(0), y(0), parent_idx(-1), cost_from_root(0.0) {}
+};
+
+class RRTStar
+{
+public:
+  RRTStar(
+    double step_size,
+    int max_iterations,
+    double goal_bias,
+    double goal_threshold,
+    double safety_dist,
+    double cost_weight,
+    int max_optimize_iters,
+    double eta);
+
+  bool planPath(
+    double start_x, double start_y,
+    double goal_x, double goal_y,
+    const nav2_costmap_2d::Costmap2D * costmap,
+    std::function<bool()> cancel_checker,
+    std::vector<RRTStarNode> & path_nodes);
+
+  void prunePath(
+    std::vector<RRTStarNode> & path,
+    const nav2_costmap_2d::Costmap2D * costmap);
+
+private:
+  void randomSample(
+    double & x, double & y,
+    double goal_x, double goal_y,
+    double min_x, double max_x, double min_y, double max_y);
+
+  int nearestNode(double x, double y);
+
+  std::vector<int> findNear(double x, double y);
+
+  bool collisionFree(
+    double x1, double y1, double x2, double y2,
+    const nav2_costmap_2d::Costmap2D * costmap);
+
+  double edgeCost(
+    double x1, double y1, double x2, double y2,
+    const nav2_costmap_2d::Costmap2D * costmap);
+
+  void steer(int from_idx, double toward_x, double toward_y,
+             double & new_x, double & new_y);
+
+  void rewire(
+    int new_idx, const std::vector<int> & near,
+    const nav2_costmap_2d::Costmap2D * costmap);
+
+  std::vector<RRTStarNode> tree_;
+  double step_size_;
+  int max_iterations_;
+  double goal_bias_;
+  double goal_threshold_;
+  double safety_dist_;
+  double cost_weight_;
+  int max_optimize_iters_;
+  double eta_;
+
+  std::mt19937 rng_;
+  bool goal_reached_;
+  int best_goal_node_idx_;
+};
+
+}  // namespace nav2_colregs_vo_rrt_star_planner
+
+#endif  // NAV2_COLREGS_VO_RRT_STAR_PLANNER__RRT_STAR_HPP_
---
+++ nav2_colregs_vo_rrt_star_planner/include/nav2_colregs_vo_rrt_star_planner/rrt_star_planner.hpp
@@ -0,0 +1,76 @@
+#ifndef NAV2_COLREGS_VO_RRT_STAR_PLANNER__VO_RRT_STAR_PLANNER_HPP_
+#define NAV2_COLREGS_VO_RRT_STAR_PLANNER__VO_RRT_STAR_PLANNER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "nav2_core/global_planner.hpp"
+#include "nav2_core/planner_exceptions.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_colregs_vo_rrt_star_planner/rrt_star.hpp"
+#include "nav2_colregs_msgs/srv/get_avoidance_point.hpp"
+#include "nav2_colregs_msgs/srv/get_barrier_lines.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace nav2_colregs_vo_rrt_star_planner
+{
+
+class VORRTStarPlanner : public nav2_core::GlobalPlanner
+{
+public:
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name,
+    std::shared_ptr<tf2_ros::Buffer> tf,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+
+  void cleanup() override;
+  void activate() override;
+  void deactivate() override;
+
+  nav_msgs::msg::Path createPlan(
+    const geometry_msgs::msg::PoseStamped & start,
+    const geometry_msgs::msg::PoseStamped & goal,
+    std::function<bool()> cancel_checker) override;
+
+private:
+  static nav_msgs::msg::Path linearInterpolation(
+    const std::vector<RRTStarNode> & raw_path,
+    double resolution);
+
+  rclcpp_lifecycle::LifecycleNode::WeakPtr parent_node_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  rclcpp::Clock::SharedPtr clock_;
+  rclcpp::Logger logger_{rclcpp::get_logger("VORRTStarPlanner")};
+  std::string global_frame_, name_;
+
+  std::unique_ptr<RRTStar> rrt_star_;
+  nav2_costmap_2d::Costmap2D * costmap_{nullptr};
+
+  double step_size_{1.0};
+  int max_iterations_{1000};
+  double goal_bias_{0.1};
+  double goal_threshold_{0.5};
+  double safety_dist_{0.3};
+  double cost_weight_{1.0};
+  int max_optimize_iters_{200};
+  double eta_{1.1};
+  double tolerance_{0.5};
+  bool prune_path_{true};
+
+  rclcpp::Client<nav2_colregs_msgs::srv::GetAvoidancePoint>::SharedPtr avoidance_client_;
+  rclcpp::Client<nav2_colregs_msgs::srv::GetBarrierLines>::SharedPtr barrier_client_;
+
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr
+    dyn_params_handler_;
+  rcl_interfaces::msg::SetParametersResult
+  dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters);
+};
+
+}  // namespace nav2_colregs_vo_rrt_star_planner
+
+#endif  // NAV2_COLREGS_VO_RRT_STAR_PLANNER__VO_RRT_STAR_PLANNER_HPP_
